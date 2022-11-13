2be9ae2167396ba2788820c36fdacdf954660965
Victoria <115575309+VicKurm@users.noreply.github.com>
2022-11-13 17:23:43 +0300

    Лабораторная работа 5

---
+++ Лабораторная работа 5/task_3.py
@@ -0,0 +1,6 @@
+def get_unique_list_numbers() -> list[int]:
+ from random import randint
+ return [randint(-10,  10) for _ in range(15)]
+list_unique_numbers = get_unique_list_numbers()
+print(list_unique_numbers)
+print(len(list_unique_numbers) == len(set(list_unique_numbers)))
